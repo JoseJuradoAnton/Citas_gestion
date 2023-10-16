e06b80b47dd46861222103232bbb7b5cf269415a
Jose Gonzales <pythonhack22@gmail.com>
2023-10-16 01:45:00 -0500

    Formulario1

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 const Formulario = () => {
     return (
-        <div>Formulario</div>
+        <Fragment>
+            <div className="formulario">
+
+                <h2>Crear Cita</h2>
+                <form>
+                    <label>Nombre Mascota</label>
+                    <input
+                        type="text"
+                        name='mascota'
+                        className='u-full-width'
+                        placeholder='Nombre Mascota'
+                    />
+
+                    <label>Nombre Dueño</label>
+                    <input
+                        type="text"
+                        name='propietario'
+                        className='u-full-width'
+                        placeholder='Nombre dueño de la mascota'
+                    />
+
+                    <label>fecha</label>
+                    <input
+                        type="date"
+                        name='fecha'
+                        className='u-full-width'
+                    />
+
+                    <label>Nombre Mascota</label>
+                    <input
+                        type="time"
+                        name='hola'
+                        className='u-full-width'
+                    />
+                </form>
+            </div>
+        </Fragment>
     )
 }
 
